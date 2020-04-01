c407aacad20dc40942e77d8ec1075d362f05497e
asingh164 <amardeep.singh@utoronto.ca>
2020-04-01 15:27:06 -0400

    2020-04-01

--- RAL.fastqc.script.sh
+++ RAL.fastqc.script.sh
@@ -3,12 +3,12 @@
 # This is a script to run FastQC on the fastq files for the DGRP lines sequenced
 
 # Move to directory containing fastq files
-cd ~/plas1/amardeep.singh/RNA.Seq.Data/Fastq.files
+cd /plas1/amardeep.singh/RNA.Seq.Data/fastq.files
 
 # Run fastqc on files. I set the number of threads to 30
-fastqc -t 30 -o ~/plas1/amardeep.singh/RNA.Seq.Data/Fastq.files/fastqc.output.files
+fastqc *.fastq.bz2 -t 30 -o /plas1/amardeep.singh/RNA.Seq.Data/fastqc.output.files
 
-
+## END 
 
 
 ##

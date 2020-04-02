f51497b0061c3700cb2c09196c2d3428668c0039
asingh164 <amardeep.singh@utoronto.ca>
2020-04-01 21:30:20 -0400

    2020-04-01

--- RAL.fastqc.script.sh
+++ RAL.fastqc.script.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
 
-# This is a script to run FastQC on the fastq files for the DGRP lines that were sequenced
-# After running FastQC it combines data from all runs using MultiQC
+###########################
+####  Program details:  ###
+###########################
 
-# Move to directory containing fastq files
-cd /plas1/amardeep.singh/RNA.Seq.Data/fastq.files
+#  This is a script to run FastQC on the fastq files for the DGRP lines that were sequenced  ####
+#  Run this script in the folder containing the fastq files that you want to run through FastQC
+#  After running FastQC it combines data from all runs using MultiQC
+
+####################################################################################################
+
+# First, we will create an output directory for fastqc outputs one directory above the current directory
+mkdir ../fastqc.output.files
 
 # Run fastqc on files. I set the number of threads to 30
-fastqc RAL*.fastq.bz2 -t 36 -o /plas1/amardeep.singh/RNA.Seq.Data/fastqc.output.files
+fastqc RAL*.fastq.bz2 -t 36 -o ../fastqc.output.files
 
+# Run MultiQC to aggregate FastQC outputs for all samples
+# Navigate over to fastqc output directory
+cd ../fastqc.output.files
 
-
+# Run MultiQC to aggregate fastqc outputs
+multiqc .
 
 ## END

1288febd3850c47e55e7d7b7ced56cb363785e1f
asingh164 <amardeep.singh@utoronto.ca>
2020-04-04 13:51:40 -0400

    2020-04-04

--- RAL.Trimmomatic.sh
+++ RAL.Trimmomatic.sh
@@ -9,9 +9,14 @@
 
 # This is a program that I wrote to deal with adapter content in the RNAseq fastq files from the DGRP identified as an issue after running fastqc
 # NOTE: the path to trimmomatic has been added as an alias in my bashrc. Users will need to do the same or modify code.
+# NOTE: This file uses 40 cores, may need to update based on resources available to you
 
-trimmomatic SE -phred33
+mkdir ../fastq.trimmed.files
 
+ls RAL*.fastq | nice -n 15 parallel -j 40 java -jar /plas1/amardeep.singh/apps/Trimmomatic-0.39/trimmomatic-0.39.jar SE -phred33 {} \
+          /plas1/amardeep.singh/RNA.Seq.Data/fastq.trimmed.files/trimmed.{} \
+          ILLUMINACLIP:/plas1/amardeep.singh/apps/Trimmomatic-0.39/adapters/TruSeq3-SE.fa:2:30:10
 
+echo DONE
 
 # END

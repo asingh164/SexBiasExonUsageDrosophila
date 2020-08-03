b8274c64548ea93d70eb7c11f06fbe5bd449c3e1
asingh164 <amardeep.singh@utoronto.ca>
2020-08-02 23:33:07 -0400

    2020-08-02

--- JunctionSeq.Script.R
+++ JunctionSeq.Script.R
@@ -61,7 +61,11 @@ java -Xmx10G -jar /plas1/amardeep.singh/apps/QoRTs-STABLE.jar mergeNovelSplices 
 # ---- R Code ----
 ### NOTE: Junctonseq (as of July 2020) will require that you install an older version of DESeq2 (versio 1.10.1)
 ##                https://www.bioconductor.org/packages/3.2/bioc/src/contrib/DESeq2_1.10.1.tar.gz
+## NOTE: This same script was used to independently examine differential exon usage in both whole body and brain tissue but the code only refers to a single tissue.
 
+### JunctionSeq Script for Body Tissue
+
+rm(list=ls())
 require(DESeq2)
 require(JunctionSeq)
 require(BiocParallel) # This is a package used for paralellization of jobs
@@ -75,14 +79,13 @@ require(BiocParallel) # This is a package used for paralellization of jobs
 #decoder.file$sex =  gsub("\\..*","", sex.tmp)
 #write.table(decoder.file, "/plas1/amardeep.singh/Ensembl.Dmel.Genome.Release/JunctionSeq.files/QoRTs.decoder.file.for.JunctionSeq.txt", quote = F, row.names = FALSE, col.names = TRUE, sep = "\t")
 
-rm(list=ls())
 # Loading in decoder file
 decoder <- read.table("/plas1/amardeep.singh/Ensembl.Dmel.Genome.Release/JunctionSeq.files/QoRTs.decoder.file.for.JunctionSeq.txt", header = TRUE, stringsAsFactors = FALSE)
 # Here I am subsetting only the samples that I want to use (for this analysis I started just with body tissue and only a single female replicate)
 decoder.for.junctionseq <- decoder[!(grepl("head|replicate.2", decoder$unique.ID)),]
 
 #Providing the directory for the count files:
-countFiles <- paste0("/plas1/amardeep.singh/Ensembl.Dmel.Genome.Release/JunctionSeq.files/count.files/", decoder.for.junctionseq$unique.ID, "/QC.spliceJunctionAndExonCounts.withNovel.forJunctionSeq.txt.gz")
+countFiles <- paste0("/plas1/amardeep.singh/Ensembl.Dmel.Genome.Release/JunctionSeq.files/count.files/body.only.replicate.1/", decoder.for.junctionseq$unique.ID, "/QC.spliceJunctionAndExonCounts.withNovel.forJunctionSeq.txt.gz")
 
 ###  Run the differential exon usage (DEU) analysis:
 
@@ -92,28 +95,28 @@ design.df <- data.frame(condition = factor(decoder.for.junctionseq$sex))
 # Building the count set object that JunctionSeq will analyze and add to it all of the parameters of the analysis
 count.set.object <- readJunctionSeqCounts(countfiles = countFiles,
                                           samplenames = decoder.for.junctionseq$unique.ID,
-                                          design = condition,
+                                          design = design.df,
                                           flat.gff.file = "/plas1/amardeep.singh/Ensembl.Dmel.Genome.Release/JunctionSeq.files/count.files/withNovel.forJunctionSeq.gff.gz",
-                                          nCores = 50,
+                                          nCores = 70,
                                           verbose = TRUE)
 
 # Generate size factors for normalization and load them into the count.set.object
 count.set.object <- estimateJunctionSeqSizeFactors(count.set.object)
 
 # Generate test specific dispersion estimates and load into count.set.object
-count.set.object <- estimateJunctionSeqDispersions(count.set.object, nCores = 40)
+count.set.object <- estimateJunctionSeqDispersions(count.set.object, nCores = 70)
 
 # Fit the observed dispersions to a regression to create a fitted dispersion
 count.set.object <- fitJunctionSeqDispersionFunction(count.set.object)
 
 # Perform the hypothesis tests to test for differential splice junction/exon usage (DEU)
-count.set.object <- testForDiffUsage(count.set.object, nCores = 40, )
+count.set.object <- testForDiffUsage(count.set.object, nCores = 60, )
 
 # Calculate effect sizes and parameter estimates
 count.set.object <- estimateEffectSizes(count.set.object)
 
 # Save output to file
-writeCompleteResults(count.set.object, "April23",
+writeCompleteResults(count.set.object, "/plas1/amardeep.singh/RNA.Seq.Data/JunctionSeq.Files/BodyOutput/Aug1.Body.Only",
                     gzip.output = TRUE,
                     FDR.threshold = 0.01,
                     save.allGenes = TRUE, save.sigGenes = TRUE,
@@ -124,18 +127,88 @@ writeCompleteResults(count.set.object, "April23",
                     verbose = TRUE)
 
 
+##### JunctionSeq Script for Head tissue
+
+rm(list=ls())
+require(DESeq2)
+require(JunctionSeq)
+require(BiocParallel) # This is a package used for paralellization of jobs
+
+# Load in decoder files and add fields for conditions
+
+# Run this only once to edit the decoder file
+#decoder.file =  read.delim("/plas1/amardeep.singh/Ensembl.Dmel.Genome.Release/JunctionSeq.files/QoRTs.decoder.file.txt", header = FALSE, stringsAsFactors=FALSE)
+#colnames(decoder.file) = "unique.ID"
+#sex.tmp = gsub("SAM.sorted.trimmed.RAL.....","", decoder.file$unique.ID)
+#decoder.file$sex =  gsub("\\..*","", sex.tmp)
+#write.table(decoder.file, "/plas1/amardeep.singh/Ensembl.Dmel.Genome.Release/JunctionSeq.files/QoRTs.decoder.file.for.JunctionSeq.txt", quote = F, row.names = FALSE, col.names = TRUE, sep = "\t")
+
+# Loading in decoder file
+decoder <- read.table("/plas1/amardeep.singh/Ensembl.Dmel.Genome.Release/JunctionSeq.files/QoRTs.decoder.file.for.JunctionSeq.txt", header = TRUE, stringsAsFactors = FALSE)
+# Here I am subsetting only the samples that I want to use (for this analysis I started just with body tissue and only a single female replicate)
+decoder.for.junctionseq <- decoder[!(grepl("body|replicate.2", decoder$unique.ID)),]
+
+#Providing the directory for the count files:
+countFiles <- paste0("/plas1/amardeep.singh/Ensembl.Dmel.Genome.Release/JunctionSeq.files/count.files/head.only.replicate.1/", decoder.for.junctionseq$unique.ID, "/QC.spliceJunctionAndExonCounts.withNovel.forJunctionSeq.txt.gz")
+
+###  Run the differential exon usage (DEU) analysis:
+
+# Creast a design dataframe which has a column for the condition you are testng (This will need to change when adding factors not sure how yet)
+design.df <- data.frame(condition = factor(decoder.for.junctionseq$sex))
+
+# Building the count set object that JunctionSeq will analyze and add to it all of the parameters of the analysis
+count.set.object <- readJunctionSeqCounts(countfiles = countFiles,
+                                          samplenames = decoder.for.junctionseq$unique.ID,
+                                          design = design.df,
+                                          flat.gff.file = "/plas1/amardeep.singh/Ensembl.Dmel.Genome.Release/JunctionSeq.files/count.files/withNovel.forJunctionSeq.gff.gz",
+                                          nCores = 70,
+                                          verbose = TRUE)
+
+# Generate size factors for normalization and load them into the count.set.object
+count.set.object <- estimateJunctionSeqSizeFactors(count.set.object)
+
+# Generate test specific dispersion estimates and load into count.set.object
+count.set.object <- estimateJunctionSeqDispersions(count.set.object, nCores = 70)
+
+# Fit the observed dispersions to a regression to create a fitted dispersion
+count.set.object <- fitJunctionSeqDispersionFunction(count.set.object)
+
+# Perform the hypothesis tests to test for differential splice junction/exon usage (DEU)
+count.set.object <- testForDiffUsage(count.set.object, nCores = 70, )
+
+# Calculate effect sizes and parameter estimates
+count.set.object <- estimateEffectSizes(count.set.object)
+
+# Save output to file
+writeCompleteResults(count.set.object, "/plas1/amardeep.singh/RNA.Seq.Data/JunctionSeq.Files/HeadOutput/Aug1.Head.Only",
+                    gzip.output = TRUE,
+                    FDR.threshold = 0.01,
+                    save.allGenes = TRUE, save.sigGenes = TRUE,
+                    save.fit = FALSE, save.VST = FALSE,
+                    save.bedTracks = TRUE,
+                    save.jscs = TRUE,
+                    bedtrack.format = c("BED", "GTF", "GFF3"),
+                    verbose = TRUE)
+
 
 ## Analyzing junctionseq output
 
-jseq.allgenes.gz = gzfile('/plas1/amardeep.singh/RNA.Seq.Data/JunctionSeq.Files/April23allGenes.results.txt.gz', 'rt')
-jseq.allgenes = read.delim(jseq.allgenes.gz, header = TRUE)
-length(unique((jseq.allgenes[jseq.allgenes$geneWisePadj < 0.01,])$geneID))
+jseq.allgenes.body = gzfile('/plas1/amardeep.singh/RNA.Seq.Data/JunctionSeq.Files/BodyOutput/Aug1.Body.OnlyallGenes.results.txt.gz', 'rt')
+jseq.allgenes.head = gzfile('/plas1/amardeep.singh/RNA.Seq.Data/JunctionSeq.Files/HeadOutput/Aug1.Head.OnlyallGenes.results.txt.gz', 'rt')
+
+jseq.allgenes.body = read.delim(jseq.allgenes.body, header = TRUE)
+jseq.allgenes.head  = read.delim(jseq.allgenes.head, header = TRUE)
+
+length(unique((jseq.allgenes.body[jseq.allgenes.body$geneWisePadj < 0.01,])$geneID))
+length(unique((jseq.allgenes.head[jseq.allgenes.head$geneWisePadj < 0.01,])$geneID))
 
 
 # Remove all sites where there are fewer than 5 reads mapping to a site
-jseq.allgenes.remove.low.expression = jseq.allgenes[jseq.allgenes$expr_female > 5 | jseq.allgenes$expr_male > 5, ]
-length(unique((jseq.allgenes.remove.low.expression[jseq.allgenes.remove.low.expression$geneWisePadj < 0.01,])$geneID))
+jseq.allgenes.body.remove.low.expression = jseq.allgenes.body[jseq.allgenes.body$expr_female > 20 | jseq.allgenes.body$expr_male > 20, ]
+length(unique((jseq.allgenes.body.remove.low.expression[jseq.allgenes.body.remove.low.expression$geneWisePadj < 0.01,])$geneID))
 
+jseq.allgenes.head.remove.low.expression = jseq.allgenes.head[jseq.allgenes.head$expr_female > 20 | jseq.allgenes.head$expr_male > 20, ]
+length(unique((jseq.allgenes.head.remove.low.expression[jseq.allgenes.head.remove.low.expression$geneWisePadj < 0.01,])$geneID))
 
 
 

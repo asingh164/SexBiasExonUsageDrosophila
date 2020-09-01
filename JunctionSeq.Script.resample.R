ca9e33db1cbcb2578a7e2047248860d6851c6a30
asingh164 <amardeep.singh@utoronto.ca>
2020-09-01 01:31:00 -0400

    2020-08-31

--- JunctionSeq.Script.resample.R
+++ JunctionSeq.Script.resample.R
@@ -29,7 +29,7 @@ rm(list=ls())
 # Loading in decoder file
 decoder <- read.table("/plas1/amardeep.singh/Ensembl.Dmel.Genome.Release/JunctionSeq.files/QoRTs.decoder.file.for.JunctionSeq.txt", header = TRUE, stringsAsFactors = FALSE)
 # Here I am subsetting only the samples that I want to use (for this analysis I started just with body tissue and only a single female replicate)
-decoder.for.junctionseq <- decoder[!(grepl("head|replicate.2", decoder$unique.ID)),]
+decoder.for.junctionseq <- decoder[!(grepl("body|replicate.2", decoder$unique.ID)),]
 rownames(decoder.for.junctionseq) = c(1:nrow(decoder.for.junctionseq))
 
 # Resample the condition factor (i.e., sex)
@@ -39,7 +39,7 @@ male.rows <- (decoder.for.junctionseq[decoder.for.junctionseq$sex == "male", ])$
 female.rows <- (decoder.for.junctionseq[decoder.for.junctionseq$sex == "female", ])$index
 
 #Providing the directory for the count files:
-countFiles <- paste0("/plas1/amardeep.singh/Ensembl.Dmel.Genome.Release/JunctionSeq.files/count.files/body.only.replicate.1/", decoder.for.junctionseq$unique.ID, "/QC.spliceJunctionAndExonCounts.withNovel.forJunctionSeq.txt.gz")
+countFiles <- paste0("/plas1/amardeep.singh/Ensembl.Dmel.Genome.Release/JunctionSeq.files/count.files/head.only.replicate.1/", decoder.for.junctionseq$unique.ID, "/QC.spliceJunctionAndExonCounts.withNovel.forJunctionSeq.txt.gz")
 
 
 for (i in 1:10) {
@@ -67,7 +67,7 @@ for (i in 1:10) {
   count.set.object.resample <- readJunctionSeqCounts(countfiles = countFiles,
                                                     samplenames = decoder.for.junctionseq.resample$unique.ID,
                                                     design = design.df.resample,
-                                                    flat.gff.file = "/plas1/amardeep.singh/Ensembl.Dmel.Genome.Release/JunctionSeq.files/count.files/body.only.replicate.1/withNovel.forJunctionSeq.gff.gz",
+                                                    flat.gff.file = "/plas1/amardeep.singh/Ensembl.Dmel.Genome.Release/JunctionSeq.files/count.files/head.only.replicate.1/withNovel.forJunctionSeq.gff.gz",
                                                     nCores = 50,
                                                     verbose = TRUE)
 
@@ -103,16 +103,28 @@ for (i in 1:10) {
   print(i)
 }
 
-jseq.allgenes.resampled.1 = read.delim("/plas1/amardeep.singh/RNA.Seq.Data/JunctionSeq.Files/ResampledData/ResultsFiles/resample1allGenes.results.txt", header = TRUE)
-jseq.allgenes.resampled.2 = read.delim("/plas1/amardeep.singh/RNA.Seq.Data/JunctionSeq.Files/ResampledData/ResultsFiles/resample2allGenes.results.txt", header = TRUE)
-jseq.allgenes.resampled.3 = read.delim("/plas1/amardeep.singh/RNA.Seq.Data/JunctionSeq.Files/ResampledData/ResultsFiles/resample3allGenes.results.txt", header = TRUE)
-jseq.allgenes.resampled.4 = read.delim("/plas1/amardeep.singh/RNA.Seq.Data/JunctionSeq.Files/ResampledData/ResultsFiles/resample4allGenes.results.txt", header = TRUE)
-jseq.allgenes.resampled.5 = read.delim("/plas1/amardeep.singh/RNA.Seq.Data/JunctionSeq.Files/ResampledData/ResultsFiles/resample5allGenes.results.txt", header = TRUE)
-jseq.allgenes.resampled.6 = read.delim("/plas1/amardeep.singh/RNA.Seq.Data/JunctionSeq.Files/ResampledData/ResultsFiles/resample6allGenes.results.txt", header = TRUE)
-jseq.allgenes.resampled.7 = read.delim("/plas1/amardeep.singh/RNA.Seq.Data/JunctionSeq.Files/ResampledData/ResultsFiles/resample7allGenes.results.txt", header = TRUE)
-jseq.allgenes.resampled.8 = read.delim("/plas1/amardeep.singh/RNA.Seq.Data/JunctionSeq.Files/ResampledData/ResultsFiles/resample8allGenes.results.txt", header = TRUE)
-jseq.allgenes.resampled.9 = read.delim("/plas1/amardeep.singh/RNA.Seq.Data/JunctionSeq.Files/ResampledData/ResultsFiles/resample9allGenes.results.txt", header = TRUE)
-jseq.allgenes.resampled.10 = read.delim("/plas1/amardeep.singh/RNA.Seq.Data/JunctionSeq.Files/ResampledData/ResultsFiles/resample10allGenes.results.txt", header = TRUE)
+jseq.allgenes.resampled.1 = read.delim("/plas1/amardeep.singh/RNA.Seq.Data/JunctionSeq.Files/ResampledData/resample1allGenes.results.txt", header = TRUE)
+jseq.allgenes.resampled.2 = read.delim("/plas1/amardeep.singh/RNA.Seq.Data/JunctionSeq.Files/ResampledData/resample2allGenes.results.txt", header = TRUE)
+jseq.allgenes.resampled.3 = read.delim("/plas1/amardeep.singh/RNA.Seq.Data/JunctionSeq.Files/ResampledData/resample3allGenes.results.txt", header = TRUE)
+jseq.allgenes.resampled.4 = read.delim("/plas1/amardeep.singh/RNA.Seq.Data/JunctionSeq.Files/ResampledData/resample4allGenes.results.txt", header = TRUE)
+jseq.allgenes.resampled.5 = read.delim("/plas1/amardeep.singh/RNA.Seq.Data/JunctionSeq.Files/ResampledData/resample5allGenes.results.txt", header = TRUE)
+jseq.allgenes.resampled.6 = read.delim("/plas1/amardeep.singh/RNA.Seq.Data/JunctionSeq.Files/ResampledData/resample6allGenes.results.txt", header = TRUE)
+jseq.allgenes.resampled.7 = read.delim("/plas1/amardeep.singh/RNA.Seq.Data/JunctionSeq.Files/ResampledData/resample7allGenes.results.txt", header = TRUE)
+jseq.allgenes.resampled.8 = read.delim("/plas1/amardeep.singh/RNA.Seq.Data/JunctionSeq.Files/ResampledData/resample8allGenes.results.txt", header = TRUE)
+jseq.allgenes.resampled.9 = read.delim("/plas1/amardeep.singh/RNA.Seq.Data/JunctionSeq.Files/ResampledData/resample9allGenes.results.txt", header = TRUE)
+jseq.allgenes.resampled.10 = read.delim("/plas1/amardeep.singh/RNA.Seq.Data/JunctionSeq.Files/ResampledData/resample10allGenes.results.txt", header = TRUE)
+
+# Remove all rows with NAs in them
+jseq.allgenes.resampled.1 = jseq.allgenes.resampled.1[complete.cases(jseq.allgenes.resampled.1),]
+jseq.allgenes.resampled.2 = jseq.allgenes.resampled.2[complete.cases(jseq.allgenes.resampled.2),]
+jseq.allgenes.resampled.3 = jseq.allgenes.resampled.3[complete.cases(jseq.allgenes.resampled.3),]
+jseq.allgenes.resampled.4 = jseq.allgenes.resampled.4[complete.cases(jseq.allgenes.resampled.4),]
+jseq.allgenes.resampled.5 = jseq.allgenes.resampled.5[complete.cases(jseq.allgenes.resampled.5),]
+jseq.allgenes.resampled.6 = jseq.allgenes.resampled.6[complete.cases(jseq.allgenes.resampled.6),]
+jseq.allgenes.resampled.7 = jseq.allgenes.resampled.7[complete.cases(jseq.allgenes.resampled.7),]
+jseq.allgenes.resampled.8 = jseq.allgenes.resampled.8[complete.cases(jseq.allgenes.resampled.8),]
+jseq.allgenes.resampled.9 = jseq.allgenes.resampled.9[complete.cases(jseq.allgenes.resampled.9),]
+jseq.allgenes.resampled.10 = jseq.allgenes.resampled.10[complete.cases(jseq.allgenes.resampled.10),]
 
 par(mfrow = c(3,4))
 hist(jseq.allgenes.resampled.1$pvalue)
